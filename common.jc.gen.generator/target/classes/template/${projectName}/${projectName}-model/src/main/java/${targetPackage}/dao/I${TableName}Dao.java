8ba3558678f1b1ceb3039ae38c1c1a35e91581a8
jiangchi <jiangchi@58ganji.com>
2019-06-14 09:25:43 +0800

    del target

--- common.jc.gen.generator/target/classes/template/${projectName}/${projectName}-model/src/main/java/${targetPackage}/dao/I${TableName}Dao.java
+++
@@ -1,7 +0,0 @@
-package ${targetPackage}.dao;
-
-import ${targetPackage}.model.${tbinfo.tbnameJavaType};
-
-public interface I${tbinfo.tbnameJavaType}Dao extends IBaseDao<${tbinfo.tbnameJavaType}, ${tbinfo.priType}> {
-
-}
